5b95fce8e9ed5a18fa220459ca0af471ae5d1a56
ikialo <synarcapp@gmail.com>
2022-11-09 18:16:44 +1000

    Banner Ad Added to the app

---
+++ lib/ad_helper.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get openAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/3419835294';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  // static String get rewardedAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return '<YOUR_ANDROID_REWARDED_AD_UNIT_ID>';
+  //   } else if (Platform.isIOS) {
+  //     return '<YOUR_IOS_REWARDED_AD_UNIT_ID>';
+  //   } else {
+  //     throw UnsupportedError('Unsupported platform');
+  //   }
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,12 @@ import 'package:camera/camera.dart';
 import 'package:flutter_camera_overlay/flutter_camera_overlay.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+
+import 'ad_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,11 +50,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late final TextRecognizer _textDetector;
 
+  BannerAd? _bannerAd;
+
   @override
   void initState() {
     // TODO: implement initState
     _textDetector = GoogleMlKit.vision.textRecognizer();
+
+    _initGoogleMobileAds();
+
+    BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          ad.dispose();
+        },
+      ),
+    ).load();
+
     super.initState();
+  }
+
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    return MobileAds.instance.initialize();
   }
 
   bool isNumeric(String s) {
@@ -98,78 +127,98 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: tab,
-        onTap: (value) {
-          setState(() {
-            tab = value;
-          });
-          switch (value) {
-            case (0):
-              setState(() {
-                flash = true;
-                format = OverlayFormat.simID000;
-              });
-              break;
-            case (1):
-              setState(() {
-                format = OverlayFormat.simID000;
-              });
-              break;
-            case (2):
-              setState(() {
-                flash = false;
-                format = OverlayFormat.simID000;
-              });
-              break;
-          }
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.credit_card),
-            label: 'Scan',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.contact_mail), label: 'US '),
-          BottomNavigationBarItem(icon: Icon(Icons.sim_card), label: 'Sim'),
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: FutureBuilder<List<CameraDescription>?>(
-        future: availableCameras(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data == null) {
-              return const Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'No camera founded',
-                    style: TextStyle(color: Colors.black),
-                  ));
-            }
+        home: SafeArea(
+            child: Scaffold(
+                bottomNavigationBar: BottomNavigationBar(
+                  currentIndex: tab,
+                  onTap: (value) {
+                    setState(() {
+                      tab = value;
+                    });
+                    switch (value) {
+                      case (0):
+                        setState(() {
+                          flash = true;
+                          format = OverlayFormat.simID000;
+                        });
+                        break;
+                      case (1):
+                        setState(() {
+                          format = OverlayFormat.simID000;
+                        });
+                        break;
+                      case (2):
+                        setState(() {
+                          flash = false;
+                          format = OverlayFormat.simID000;
+                        });
+                        break;
+                    }
+                  },
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.credit_card),
+                      label: 'Scan',
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.contact_mail), label: 'US '),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.sim_card), label: 'Sim'),
+                  ],
+                ),
+                backgroundColor: Colors.white,
+                body: Stack(children: [
+                  FutureBuilder<List<CameraDescription>?>(
+                    future: availableCameras(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data == null) {
+                          return const Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'No camera founded',
+                                style: TextStyle(color: Colors.black),
+                              ));
+                        }
 
-            return CameraOverlay(
-              snapshot.data!.first,
-              CardOverlay.byFormat(format),
-              (XFile file) {
-                print(File(file.path));
-                _recognizTexts(file.path);
-              },
-              info:
-                  'Position your  Prepid card within the rectangle and ensure the image is perfectly readable.',
-              label: 'Scanning Prepaid Card',
-              flash: flash,
-            );
-          } else {
-            return const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'Fetching cameras',
-                  style: TextStyle(color: Colors.black),
-                ));
-          }
-        },
-      ),
-    ));
+                        return CameraOverlay(
+                          snapshot.data!.first,
+                          CardOverlay.byFormat(format),
+                          (XFile file) {
+                            print(File(file.path));
+                            _recognizTexts(file.path);
+                          },
+                          info:
+                              'Position your  Prepid card within the rectangle and ensure the image is perfectly readable.',
+                          label: 'Scanning Prepaid Card',
+                          flash: flash,
+                        );
+                      } else {
+                        return const Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Fetching cameras',
+                              style: TextStyle(color: Colors.black),
+                            ));
+                      }
+                    },
+                  ),
+                  if (_bannerAd != null)
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: _bannerAd!.size.width.toDouble(),
+                        height: _bannerAd!.size.height.toDouble(),
+                        child: AdWidget(ad: _bannerAd!),
+                      ),
+                    ),
+                ]))));
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+
+    super.dispose();
   }
 }
