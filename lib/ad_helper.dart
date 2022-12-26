b0b71930ab825d26e6bf984c801ac2c7b4fdffd9
ikialo <synarcapp@gmail.com>
2022-12-26 21:27:56 +1000

    icon change

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      //return 'ca-app-pub-3940256099942544/6300978111';
-      return 'ca-app-pub-2931554353232213/3986300368';
+      return 'ca-app-pub-3940256099942544/6300978111';
+      //return 'ca-app-pub-2931554353232213/3986300368';
     } else if (Platform.isIOS) {
       return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
     } else {
@@ -24,14 +24,12 @@ class AdHelper {
 
   static String get intetsititalAd {
     if (Platform.isAndroid) {
-      //return 'ca-app-pub-3940256099942544/1033173712';
-      return 'ca-app-pub-2931554353232213/3280002592';
+      return 'ca-app-pub-3940256099942544/1033173712';
+      //return 'ca-app-pub-2931554353232213/3280002592';
     } else if (Platform.isIOS) {
       return '<YOUR_IOS_REWARDED_AD_UNIT_ID>';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
   }
-
-
 }
